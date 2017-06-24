96faabf8310eee88745a935aa1aafe94fd3d3f74
VenusPP <532262403@qq.com>
2017-06-24 15:56:43 +0800

    第一次提交

---
+++ mongdb--demo.bat
@@ -0,0 +1,8 @@
+@REM @Author: Administrator
+@REM @Date:   2017-06-23 22:58:50
+@REM @Last Modified by:   Administrator
+@REM Modified time: 2017-06-24 00:27:41
+
+ D:\mgondb\MongoDB\Server\3.4\bin\mongod.exe --logappend --install --directoryperdb --serviceName MongoDB --serviceDisplayName MongoDB --logpath=D:\MongdbProducts\logs\log.txt --dbpath=D:\MongdbProducts\data
+
+ pause:
---
+++ mongdb--start.bat
@@ -0,0 +1,6 @@
+@REM @Author: Administrator
+@REM @Date:   2017-06-24 00:31:31
+@REM @Last Modified by:   Administrator
+@REM Modified time: 2017-06-24 00:33:49
+
+D:\mgondb\MongoDB\Server\3.4\bin\mongo.exe
